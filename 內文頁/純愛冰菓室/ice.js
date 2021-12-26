9f9e787edba2a63c37587b56d73b8878ea3da2dc
ie0728 <96630318+ie0728@users.noreply.github.com>
2021-12-26 22:25:10 +0800

    Add files via upload

---
+++ 內文頁/純愛冰菓室/ice.js
@@ -0,0 +1,16 @@
+function pageLoad() {
+  var menubutton = document.getElementById("mobile-menu");
+  var flag = 0;
+  menubutton.onclick = menuonandoff;
+
+  function menuonandoff() {
+    var menuitem = document.getElementById("mobilenavbar");
+    if (flag == 0) {
+      menuitem.style.display = "block";
+    } else {
+      menuitem.style.display = "none";
+    }
+    flag = !flag;
+  }
+}
+window.onload = pageLoad;
